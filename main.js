e0623b5409f692688791f9b107d165ed8c7bde90
Hanocha <saiki@feedforce.jp>
2019-07-11 18:44:34 +0900

    Show link to holiday request page under the rest signs

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 window.onload = () => {
-  console.log("test");
+  const targetElements = []
+  const delayedElements = document.querySelectorAll("font[style='font-weight: bold;color: #ff8c00;']");
+  const absenceElements = document.querySelectorAll("font[style='font-weight: bold;color: black;']");
+
+  targetElements.push(...Array.from(delayedElements));
+  targetElements.push(...Array.from(absenceElements));
+
+  targetElements.forEach(element => {
+    const dateArray = [...element.parentElement.parentElement.children[0].children[0].textContent.matchAll(/\d\d/g)];
+    const month = parseInt(dateArray[0]);
+    const day = parseInt(dateArray[1]);
+
+    const holidayRequestText = document.createTextNode("有給申請");
+
+    const linkToHolidayRequest = document.createElement("a");
+    linkToHolidayRequest.setAttribute("href", `/employee/holiday/new?holiday_month=${month}&holiday_day=${day}&holiday_id=45`);
+    linkToHolidayRequest.appendChild(holidayRequestText);
+
+    const holidayRequestContainer = document.createElement("div");
+    holidayRequestContainer.appendChild(linkToHolidayRequest);
+
+    element.parentElement.appendChild(holidayRequestContainer);
+  })
 };
